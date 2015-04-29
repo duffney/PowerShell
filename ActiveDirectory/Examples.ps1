f82e8eebf4ed63914d0628ccc6ef7a03d86734a3
Joshua Duffney <duffney@outlook.com>
2015-04-29 12:57:38 -0500

    Update Examples.ps1

--- ActiveDirectory/Examples.ps1
+++ ActiveDirectory/Examples.ps1
@@ -3,3 +3,8 @@ Get-ADuser –filter * -properties * | ft name,department
 Get-ADUser –Filter * -Properties * | sort –Property Department | ft Name,Department
 
 Get-ADUser –filter {Department –eq “Sales” –or Department –eq “Marketing”} –Properties * | ft –Property Surname,Department,PasswordLastSet
+
+Get-ADComputer –filter * | % {Get-hotfix –computername $PSItem.Name}
+
+Get-ADComputer –filter * | % {Invoke-command $PSitem.Name –scriptblock { get-hotfix}}
+
