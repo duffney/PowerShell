ecdace1efc982c29f819632a2c49827e377524f9
Joshua Duffney <duffney@outlook.com>
2015-04-29 09:00:40 -0500

    Update Examples.ps1

--- WMI/Examples.ps1
+++ WMI/Examples.ps1
@@ -10,5 +10,4 @@ Get-WmiObject -Class Win32_Service -Filter "Name LIKE 'S%'" | select Name,State,
  
 Get-WmiObject -class Win32_service -Filter "Name='WinRM'" | Invoke-WmiMethod -Name ChangeStartMode -Argument 'Automatic' 
 
-
 Get-WmiObject Win32_LogicalDisk -Filter "DriveType=3" | Format-Table -Property @{n='Drive Letter';e={$PSItem.DeviceID}},@{n='Free Space(GB)';e={$PSItem.FreeSpace / 1GB};formatstring='N0'},@{n='% Free Space';e={$PSItem.FreeSpace / $PSitem.Size * 100};formatstring='N0'}
