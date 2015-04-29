e5be4de24ac8da8a85923b122549cb62db11ace4
Joshua Duffney <duffney@outlook.com>
2015-04-29 09:00:32 -0500

    Update Examples.ps1

--- WMI/Examples.ps1
+++ WMI/Examples.ps1
@@ -1,9 +1,11 @@
 Get-WmiObject -class Win32_LogicalDisk -Filter "DriveType=3" 
- 
+
 Get-WmiObject -Namespace root\cimv2 -list | where name -like "*configuration*" | sort name 
  
 Get-WmiObject -Namespace root\cimv2 -list | where name -like "*operating*" | sort name 
+
 Get-WmiObject -class Win32_ComputerSystem | Select Manufacturer,Model,@{n='RAM';e={$PSItem.TotalPhysicalMemory}} 
+
 Get-WmiObject -Class Win32_Service -Filter "Name LIKE 'S%'" | select Name,State,StartName 
  
 Get-WmiObject -class Win32_service -Filter "Name='WinRM'" | Invoke-WmiMethod -Name ChangeStartMode -Argument 'Automatic' 
