b0af12dddf9f6ab5609b53658183972811075cc6
Joshua Duffney <duffney@outlook.com>
2015-06-25 14:16:10 -0500

    Update Advanced_Function_Template.ps1

--- Tips-Tricks/Advanced_Function_Template.ps1
+++ Tips-Tricks/Advanced_Function_Template.ps1
@@ -13,3 +13,27 @@ function <name> {
     PROCESS {}
     END {}
 }
+
+
+Function set-function {
+[cmdletbinding(SupportsShouldProcess=$True,ConfirmImpact="High")]
+
+Param (
+[Parameter(Position=0,Mandatory=$True,HelpMessage="Enter a computername")]
+[ValidateNotNullorEmpty()]
+[string]$Param
+
+)
+Begin {
+
+} #Begin
+
+Process {
+    
+} #Process
+
+End {
+
+} #end
+
+}
