35af96c2eecf880dc817c124cc9b9115e7f7ce92
Joshua Duffney <duffney@outlook.com>
2015-08-21 10:18:14 -0500

    Update Disable-ADComputer.ps1
    Added parameters for multi domain support and appending description of the computer object.

--- ActiveDirectory/Disable-ADComputer.ps1
+++ ActiveDirectory/Disable-ADComputer.ps1
@@ -9,13 +9,20 @@ then outputs errors to a log file.
 Name of computer or computers
 .PARAMETER DisabledOU
 Specifies the Distinguished Name of the OU the device will be moved to.
+.PARAMETER Domain
+Specifies the domain in which to query for the computer given to the ComputerName parameter. If left blank it will query the domain of the currently logged on user.
+.PARAMETER Description
+By default this will append to the existing description of the computer object and append any text given to this parameter.
 .EXAMPLE
-Disable-ADComputer -ComputerName computer1 -DisabledOU 'OU=Computers,OU=Disabled Accounts,DC=domain,DC=com' -Verbose
+Disable-ADComputer -ComputerName computer1 -DisabledOU 'OU=Computers,OU=Disabled Accounts,DC=domain,DC=com' -Description "CR00001' -Domain domain.forest.com -Verbose -whatif
 #>
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Medium')]
     param(
+        [Parameter(Mandatory = $true, ValueFromPipeline=$true)]
         [string[]]$ComputerName,
         [string]$DisabledOU,
+        [string]$Domain = (Get-ADDomain).DNSroot,
+        [string]$Description,
         [string]$ErrorLog = 'c:\retry.txt'
     )
     BEGIN {
@@ -25,11 +32,17 @@ Disable-ADComputer -ComputerName computer1 -DisabledOU 'OU=Computers,OU=Disabled
         foreach($Computer in $ComputerName){
             Try {
                 Write-Verbose "Disabling $Computer"
-                Set-ADComputer -Identity $Computer -Enabled $false
+                
+                $ExistingDescription = (Get-ADComputer -Identity $Computer -Server $Domain -Properties Description).Description
+                $Description = $ExistingDescription + " "+ $Description
+                
+                Set-ADComputer -Identity $Computer -Description $Description -Enabled $false -Server $Domain
 
                 Write-Verbose "Moving $Computer to $DisabledOU"
-                $DistinguishedName = (Get-ADComputer -Identity $Computer).DistinguishedName
-                Move-ADObject -Identity $DistinguishedName -TargetPath $DisabledOU -Verbose
+
+                $DistinguishedName = (Get-ADComputer -Identity $Computer -Server $Domain).DistinguishedName
+                Move-ADObject -Identity $DistinguishedName -TargetPath $DisabledOU -Server $Domain -Verbose
+                
             }
             Catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]{
                 Write-Warning "$Computer was not found"
