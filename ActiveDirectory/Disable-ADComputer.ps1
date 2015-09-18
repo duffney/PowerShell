e72ddc71fc87d6f8e616d4f3c709489df9fc302b
Joshua Duffney <duffney@outlook.com>
2015-09-18 08:41:01 -0500

    Added Commet based help

--- ActiveDirectory/Disable-ADComputer.ps1
+++ ActiveDirectory/Disable-ADComputer.ps1
@@ -1,58 +1,61 @@
-function Disable-ADComputer {
+function Copy-SQLTable {
 <#
 .SYNOPSIS
-Disables Computers and moves them to the specified OU.
+Copies a table from a source database and inserts it into the target database.
 .DESCRIPTION
-Take string or object input for computers then disables each one and moves to the specified ou
-then outputs errors to a log file.
-.PARAMETER COMPUTERNAME
-Name of computer or computers
-.PARAMETER DisabledOU
-Specifies the Distinguished Name of the OU the device will be moved to.
-.PARAMETER Domain
-Specifies the domain in which to query for the computer given to the ComputerName parameter. If left blank it will query the domain of the currently logged on user.
-.PARAMETER Description
-By default this will append to the existing description of the computer object and append any text given to this parameter.
+Takes all the data from the source database and inserts it into the target database with the same name.
+.PARAMETER TableName
+Specifies the table name to be copied from the source and inserted into the target.
+.PARAMETER SourceServer
+Specifies source server DNS name and instance, use ServerName\InstanceName.
+.PARAMETER SourceDataBase
+Specifies the source database name.
+.PARAMETER TargetDatabase
+Specifies the target database name.
+.PARAMETER TargetServer
+SPecifies the target server DNS name and instance, user ServerName\InstanceName.
 .EXAMPLE
 Disable-ADComputer -ComputerName computer1 -DisabledOU 'OU=Computers,OU=Disabled Accounts,DC=domain,DC=com' -Description "CR00001' -Domain domain.forest.com -Verbose -whatif
 #>
-    [CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Medium')]
-    param(
-        [Parameter(Mandatory = $true, ValueFromPipeline=$true)]
-        [string[]]$ComputerName,
-        [string]$DisabledOU,
-        [string]$Domain = (Get-ADDomain).DNSroot,
-        [string]$Description,
-        [string]$ErrorLog = 'c:\retry.txt'
-    )
-    BEGIN {
-        Write-Verbose "Starting Disable-ADComputer"
-    }
-    PROCESS {
-        foreach($Computer in $ComputerName){
-            Try {
-                $ComputerInfo = Get-ADComputer -Identity $Computer -Server $Domain -Properties Description
-                $Description = $($ComputerInfo.Description) + " "+ $Description
+[CmdletBinding()]
 
-                Write-Verbose "Disabling $Computer"
-                
-                Set-ADComputer -Identity $Computer -Description $Description -Enabled $false -Server $Domain -Verbose
+Param(
 
-                Write-Verbose "Moving $Computer to $DisabledOU"
+[string]$TableName,
 
-                Move-ADObject -Identity $($ComputerInfo.DistinguishedName) -TargetPath $DisabledOU -Server $Domain -Verbose
-                
+[string]$SourceServer,
+
+[string]$SourceDataBase,
+
+[string]$TargetDatabase,
+
+[string]$TargetServer
+)
+
+
+$SourceConnectionString = "server=$SourceServer;database=$SourceDataBase;trusted_connection=true"
+$TargetConnectionString = "server=$TargetServer;database=$TargetDatabase;trusted_connection=true"
+
+$Data = Get-DatabaseData -connectionString $SourceConnectionString -query "Select * from $TableName" -isSQLServer
+$Data = $Data[1..($Data.Count)]
+
+$Columns = (Get-DatabaseData -connectionString $SourceConnectionString -query "SELECT COLUMN_NAME FROM $SourceDataBase.information_schema.columns WHERE  table_name = '$TableName' ORDER  BY ORDINAL_POSITION" -isSQLServer).Column_Name
+
+    foreach ($c in $Columns){$InsertColumns += $c+','}
+
+$InsertColumns=$InsertColumns.Trim(',')
+
+    Foreach ($Value in $Data) {
+
+            $array = $Value.ItemArray
+            foreach ($a in $array){
+                $values += "'"+$a+"'"+','
             }
-            Catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]{
-                Write-Warning "$Computer was not found"
-                Write-Output $Computer | Out-File $ErrorLog
-            }
-            Catch {
-                Write-Warning $_.Exception.Message
-            }
-        }
-    }
-    END {
-        Write-Verbose "Finished"
+            $Values = $Values.Trim(',')
+            $query = "Insert Into $TableName ($InsertColumns) Values ($Values)"
+            $Values = $null
+
+
+        Invoke-DatabaseQuery -connectionString $TargetConnectionString -query $query -isSQLServer
     }
 }
