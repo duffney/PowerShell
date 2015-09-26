47024f18eecc0019ce8cee4bed8d7e0773760d65
Joshua Duffney <duffney@outlook.com>
2015-09-26 09:41:20 -0500

    Update Set-NetworkSettings.ps1

--- Hyper-V/Set-NetworkSettings.ps1
+++ Hyper-V/Set-NetworkSettings.ps1
@@ -4,3 +4,6 @@ Get-NetAdapter | select Name,InterfaceAlias,InterfaceIndex
 New-NetIPAddress –InterfaceAlias "Ethernet" –PrefixLength 24 –DefaultGateway 192.168.16.1 -IPAddress 192.168.1.105
 #Set DNS
 Set-DNSClientServerAddress –InterfaceAlias "Ethernet" –ServerAddress 192.168.16.10, 192.168.16.11 
+
+#Reference
+#http://www.falconitservices.com/support/KB/Lists/Posts/Post.aspx?ID=100
