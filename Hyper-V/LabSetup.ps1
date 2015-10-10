d943572f1e247e7fa5c25cce9a75a4c5368cb397
Joshua Duffney <duffney@outlook.com>
2015-10-10 09:33:48 -0500

    Cmdlet for child domain controller

--- Hyper-V/LabSetup.ps1
+++ Hyper-V/LabSetup.ps1
@@ -2,3 +2,4 @@ Get-NetAdapter | Select Name,MacAddress #Select NetAdpter with internet access
 New-VMSwitch -NetAdapterName 'Wi-Fi' -Name 'External' #Create External internet providing Hyper-v adapter
 New-VMSwitch -SwitchType Internal -Name 'Internal'
 
+Install-ADDSDomain -Credential (Get-Credential manticore\zephyr) -NewDomainName Hydra -ParentDomainName manticore.org -SkipPreChecks -SafeModeAdministratorPassword (($PWD = '')|ConvertTo-SecureString -AsPlainText -Force) -DomainType ChildDomain -NewDomainNetbiosName hydra
