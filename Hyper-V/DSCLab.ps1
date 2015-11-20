2e35b36ed16d037b72b1d610eb42164f514d8ae9
Joshua Duffney <duffney@outlook.com>
2015-11-20 13:20:19 -0600

    initial commits

---
+++ Hyper-V/DSCLab.ps1
@@ -0,0 +1,154 @@
+﻿configuration LabDomain             
+{             
+   param             
+    (             
+        [Parameter(Mandatory)]             
+        [pscredential]$safemodeAdministratorCred,             
+        [Parameter(Mandatory)]            
+        [pscredential]$domainCred,
+        [Parameter(Mandatory)][string]$NewName                  
+    )             
+            
+    Import-DscResource -ModuleName xActiveDirectory
+    Import-DscResource –ModuleName PSDesiredStateConfiguration
+    Import-DscResource -Module xNetworking
+    Import-DscResource -module xDHCpServer
+    Import-DscResource -Module xComputerManagement 
+ 
+            
+    Node $AllNodes.Where{$_.Role -eq "Primary DC"}.Nodename             
+    {             
+            
+        LocalConfigurationManager            
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        }
+        
+        xComputer NewName
+        {
+            Name = $NewName
+ 
+        }
+        
+        xIPAddress NewIPAddress
+        {
+            IPAddress      = "192.168.2.30"
+            InterfaceAlias = "Ethernet"
+            SubnetMask     = 24
+            AddressFamily  = "IPV4"
+ 
+        }
+
+        xDefaultGatewayAddress NewDefaultGateway
+        {
+            AddressFamily = 'IPv4'
+            InterfaceAlias = 'Ethernet'
+            Address = '192.168.2.1'
+            DependsOn = '[xIPAddress]NewIpAddress'
+
+        }
+
+        
+        WindowsFeature DHCP {
+            DependsOn = '[xIPAddress]NewIpAddress'
+            Name = 'DHCP'
+            Ensure = 'PRESENT'
+            IncludeAllSubFeature = $true                                                                                                                              
+ 
+        }  
+ 
+        WindowsFeature DHCPTools
+        {
+            DependsOn= '[WindowsFeature]DHCP'
+            Ensure = 'Present'
+            Name = 'RSAT-DHCP'
+            IncludeAllSubFeature = $true
+        }  
+                                     
+            
+        File ADFiles            
+        {            
+            DestinationPath = 'C:\NTDS'            
+            Type = 'Directory'            
+            Ensure = 'Present'            
+        }            
+                    
+        WindowsFeature ADDSInstall             
+        {             
+            Ensure = "Present"             
+            Name = "AD-Domain-Services"
+            IncludeAllSubFeature = $true
+             
+        }            
+            
+        # Optional GUI tools            
+        WindowsFeature ADDSTools            
+        {             
+            Ensure = "Present"             
+            Name = "RSAT-ADDS"             
+        }            
+            
+        # No slash at end of folder paths            
+        xADDomain FirstDS             
+        {             
+            DomainName = $Node.DomainName             
+            DomainAdministratorCredential = $domainCred             
+            SafemodeAdministratorPassword = $safemodeAdministratorCred            
+            DatabasePath = 'C:\NTDS'            
+            LogPath = 'C:\NTDS'            
+            DependsOn = "[WindowsFeature]ADDSInstall","[File]ADFiles"            
+        }
+        
+        xDhcpServerScope Scope
+        {
+         DependsOn = '[WindowsFeature]DHCP'
+         Ensure = 'Present'
+         IPEndRange = '192.168.2.200'
+         IPStartRange = '192.168.2.100'
+         Name = 'PowerShellScope'
+         SubnetMask = '255.255.255.0'
+         LeaseDuration = '00:08:00'
+         State = 'Active'
+         AddressFamily = 'IPv4'
+        } 
+ 
+        xDhcpServerOption Option
+     {
+         Ensure = 'Present'
+         ScopeID = '192.168.2.0'
+         DnsDomain = 'zephyr.org'
+         DnsServerIPAddress = '192.168.2.30'
+         AddressFamily = 'IPv4'
+     }                    
+            
+    }             
+}
+# Configuration Data for AD              
+$ConfigData = @{             
+    AllNodes = @(             
+        @{             
+            Nodename = $env:computername             
+            Role = "Primary DC"             
+            DomainName = "Zephyr.org"             
+            RetryCount = 20              
+            RetryIntervalSec = 30            
+            PsDscAllowPlainTextPassword = $true
+            PSDscAllowDomainUser = $true            
+        }            
+    )             
+}             
+            
+LabDomain -ConfigurationData $ConfigData `
+    -safemodeAdministratorCred (Get-Credential -UserName '(Password Only)' `
+        -Message "New Domain Safe Mode Administrator Password") `
+    -domainCred (Get-Credential -UserName Zephyr\administrator `
+        -Message "New Domain Admin Credential") `
+    -NewName 'S1'            
+            
+# Make sure that LCM is set to continue configuration after reboot            
+Set-DSCLocalConfigurationManager -Path C:\DSC\LabDomain\ –Verbose            
+            
+# Build the domain            
+Start-DscConfiguration -Wait -Force -Path C:\DSC\LabDomain\ -Verbose 
---
+++ Hyper-V/DSCLabSetup.ps1
@@ -0,0 +1,31 @@
+﻿$Name = 'S1'
+$SwitchName = 'External'
+$HardDiskSize = 32GB
+$HDPath = 'F:\Hyper-V\Virtual Hard Disks'+'\'+$Name+'.vhdx'
+$Generation = '2'
+$ISO_Path = 'D:\ISOs\10514.0.150808-1529.TH2_RELEASE_SERVER_OEMRET_X64FRE_EN-US.ISO'
+
+New-VM -Name $Name -SwitchName $SwitchName `
+-NewVHDSizeBytes $HardDiskSize `
+-NewVHDPath $HDPath -Generation $Generation -MemoryStartupBytes 1024MB
+
+Add-VMDvdDrive -VMName $Name -Path $ISO_Path
+
+
+$MyDVD = Get-VMDvdDrive $Name
+$MyHD = Get-VMHardDiskDrive $Name
+$MyNIC = Get-VMNetworkAdapter $Name
+
+Set-VMFirmware $Name -BootOrder $MyDVD,$MyHD,$MyNIC
+Set-VMMemory $Name -DynamicMemoryEnabled $false
+break
+
+#Image Server
+
+Save-Module -Name 'xNetworking','xDHCPServer','xComputerManagement','xActiveDirectory','xPSDesiredStateConfiguration' -Path 'C:\LabResources'
+#Create .iso
+
+$LabResourcePath = 'C:\LabResources\LabResources.iso'
+Set-VMDvdDrive -VMName $Name -Path $LabResourcePath
+
+Invoke-Command -VMName $Name -ScriptBlock {Copy-Item -Path D:\* -Recurse -Destination "$env:ProgramFiles\WindowsPowerShell\Modules" -Force}
