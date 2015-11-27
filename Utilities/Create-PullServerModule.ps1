b10b647c107baa49d13f23da549cb76d604f56e2
Joshua Duffney <duffney@outlook.com>
2015-11-26 21:16:58 -0600

    Initial commi

---
+++ Utilities/Create-PullServerModule.ps1
@@ -0,0 +1,23 @@
+﻿$source = "C:\LabResources\xTimeZone"
+
+$destination = "C:\temp"
+
+$Version = (Get-ChildItem -Path $source -Depth 1).Name
+$ResoureName = (Get-ChildItem -Path $source -Depth 1).Parent.Name
+$ModuleName = $ResoureName+'_'+$Version
+
+New-Item -Path ($destination+'\'+$ModuleName) -ItemType Directory
+
+Get-ChildItem ($source+'\'+$Version) | Copy-Item -Destination ($destination+'\'+$ModuleName)
+
+$destinationZip = ($destination+'\'+$ModuleName)+'.zip'
+
+
+
+ If(Test-path $destinationZip) {Remove-item $destinationZip -Force}
+
+Add-Type -assembly "system.io.compression.filesystem"
+
+[io.compression.zipfile]::CreateFromDirectory(($destination+'\'+$ModuleName), $destinationZip)
+
+Remove-Item -Path ($destination+'\'+$ModuleName) -Force
