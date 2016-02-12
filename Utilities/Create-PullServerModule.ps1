976244977d097015973ba083a15c362d79c21a40
Joshua Duffney <duffney@outlook.com>
2016-02-12 12:41:40 -0600

    actually works

--- Utilities/Create-PullServerModule.ps1
+++ Utilities/Create-PullServerModule.ps1
@@ -1,14 +1,14 @@
-﻿$source = "C:\LabResources\xTimeZone"
+﻿$source = "C:\Program Files\WindowsPowerShell\Modules\xTimeZone"
 
-$destination = "C:\temp"
+$destination = "$Env:PROGRAMFILES\WindowsPowerShell\DscService\Modules\"
 
-$Version = (Get-ChildItem -Path $source -Depth 1).Name
-$ResoureName = (Get-ChildItem -Path $source -Depth 1).Parent.Name
+$Version = (Get-ChildItem -Path $source -Depth 1).Name[0]
+$ResoureName = (Get-ChildItem -Path $source -Depth 1).Parent.Name[0]
 $ModuleName = $ResoureName+'_'+$Version
 
 New-Item -Path ($destination+'\'+$ModuleName) -ItemType Directory
 
-Get-ChildItem ($source+'\'+$Version) | Copy-Item -Destination ($destination+'\'+$ModuleName)
+Get-ChildItem ($source+'\'+$Version) | Copy-Item -Destination ($destination+'\'+$ModuleName) -Recurse
 
 $destinationZip = ($destination+'\'+$ModuleName)+'.zip'
 
@@ -21,3 +21,5 @@ Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::CreateFromDirectory(($destination+'\'+$ModuleName), $destinationZip)
 
 Remove-Item -Path ($destination+'\'+$ModuleName) -Force
+
+New-DscCheckSum -ConfigurationPath $destinationZip -OutPath $destination -Verbose -Force
