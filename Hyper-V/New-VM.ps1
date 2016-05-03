b17de145cc3254cb4515efc7545963286b3c87c5
Joshua Duffney <duffney@outlook.com>
2016-05-03 07:46:40 -0500

    added enable-nestedvm

--- Hyper-V/New-VM.ps1
+++ Hyper-V/New-VM.ps1
@@ -1,16 +1,17 @@
-$Name = 'DSC'
+$Name = 'Hyper-V'
 $SwitchName = 'Internal'
 $HardDiskSize = 32GB
 $HDPath = 'E:\Hyper-V\Virtual Hard Disks'+'\'+$Name+'.vhdx'
 $Generation = '2'
-$ISO_Path = 'D:\ISOs\Windows Server 2016\WindowsServer2016TechnicalPreview4.ISO'
+$ISO_Path = 'D:\ISOs\Windows Server 2016\WindowsServer2016TP5.ISO'
 
 New-VM -Name $Name -SwitchName $SwitchName `
 -NewVHDSizeBytes $HardDiskSize `
--NewVHDPath $HDPath -Generation $Generation -MemoryStartupBytes 1024MB
+-NewVHDPath $HDPath -Generation $Generation -MemoryStartupBytes 4096MB -Verbose
 
-Add-VMDvdDrive -VMName $Name -Path $ISO_Path
+Add-VMDvdDrive -VMName $Name -Path $ISO_Path -Verbose
 
+set-vm -ProcessorCount 2 -VMName $Name -Verbose
 
 $MyDVD = Get-VMDvdDrive $Name
 $MyHD = Get-VMHardDiskDrive $Name
@@ -20,6 +21,3 @@ Set-VMFirmware $Name -BootOrder $MyDVD,$MyHD,$MyNIC
 Set-VMMemory $Name -DynamicMemoryEnabled $false
 
 break
-
-#New-NetIPAddress �InterfaceIndex 12 �IPAddress -192.0.2.2 �PrefixLength 24 �DefaultGateway -192.0.2.1
-#Get-NetFirewallProfile | Set-NetFirewallProfile -Enabled false #disable firewall
