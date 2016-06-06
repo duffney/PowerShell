3b50c94e22710726f3410cb7f498be1475f3e916
Joshua Duffney <duffney@outlook.com>
2016-06-06 08:13:39 -0500

    Add Computers to groups in PowerShell

--- ActiveDirectory/Examples.ps1
+++ ActiveDirectory/Examples.ps1
@@ -9,5 +9,6 @@ Get-ADComputer –filter * | % {Get-hotfix –computername $PSItem.Name}
 Get-ADComputer –filter * | % {Invoke-command $PSitem.Name –scriptblock { get-hotfix}}
 
 Get-ADGroupMember -Identity 'domain controllers' | % {Get-ADComputer $PSItem.Name -Properties OperatingSystem} | select Name,OperatingSystem
-
+#Add Computers to groups in PowerShell
+Set-ADGroup -Add:@{'Member'="CN=WEFS1,CN=Computers,DC=WEF,DC=COM"} -Identity:"CN=Event Log Readers,CN=Builtin,DC=WEF,DC=COM" -Server:"WEFDC.WEF.COM"
 help about_active*
