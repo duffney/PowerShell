643e92348f32c9319890f3fe35570bc8cbfeda46
Joshua Duffney <duffney@outlook.com>
2016-09-02 10:40:22 -0500

    Update HashTables.ps1

--- Tips-Tricks/HashTables.ps1
+++ Tips-Tricks/HashTables.ps1
@@ -10,3 +10,7 @@
 $os = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computer
 $hash.Add("Version",$os.Version)
 $hash.Add("ServicePackMajorVersion",$os.ServicePackMajorVersion)
+
+#add values to properties that dont exist
+$splat = @{Properties=('Managedby','whencreated');Identity='testgroup';server='domain.com'}
+if(!$splat.Properties.Contains('member')){$splat.Properties = $splat.Properties += 'member'}
