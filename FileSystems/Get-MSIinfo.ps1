36554376054a9df8e39223877470b6e860da6376
evfedoro <Evgenii.Fedorov@dhl.com>
2018-02-16 14:30:59 +0100

    removed an empty object created in pipeline in line 44

--- FileSystems/Get-MSIinfo.ps1
+++ FileSystems/Get-MSIinfo.ps1
@@ -41,11 +41,11 @@ function Get-MSIinfo {
             $MSIDatabase = $WindowsInstaller.GetType().InvokeMember("OpenDatabase","InvokeMethod",$Null,$WindowsInstaller,@($Path.FullName,0))
             $Query = "SELECT Value FROM Property WHERE Property = '$($Property)'"
             $View = $MSIDatabase.GetType().InvokeMember("OpenView","InvokeMethod",$null,$MSIDatabase,($Query))
-            $View.GetType().InvokeMember("Execute", "InvokeMethod", $null, $View, $null)
+            $null = $View.GetType().InvokeMember("Execute", "InvokeMethod", $null, $View, $null)
             $Record = $View.GetType().InvokeMember("Fetch","InvokeMethod",$null,$View,$null)
             $Value = $Record.GetType().InvokeMember("StringData","GetProperty",$null,$Record,1)
             return $Value
-        } 
+        }
         catch {
             Write-Output $_.Exception.Message
         }
